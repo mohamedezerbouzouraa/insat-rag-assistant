7495e4fb617ce09adce9dd6e43ae1983610f792a
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-05-28 11:36:10 +0100

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.chat_routes import chat_bp
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(chat_bp)
+
+if __name__ == "__main__":
+    app.run(debug=False, port=5000)
